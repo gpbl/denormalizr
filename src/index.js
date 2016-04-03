b2bb4bdca0cd96089b1626fe6b40b4a8cfd496a9
YoruNoHikage <yorunohikage@gmail.com>
2016-04-03 19:36:59 +0200

    Fix the interdependency issue

--- src/index.js
+++ src/index.js
@@ -3,29 +3,46 @@ import EntitySchema from 'normalizr/lib/EntitySchema';
 import UnionSchema from 'normalizr/lib/UnionSchema';
 import merge from "lodash/merge";
 
-function denormalizeArray(items, entities, schema) {
-  const itemSchema = schema.getItemSchema();
-  const itemKey = itemSchema.getKey();
-  return items.map(id => {
-    const item = entities[itemKey][id];
-    return denormalize(item, entities, itemSchema);
-  });
+function getItem(id, key, schema, entities, bag) {
+  if(!bag[key]) {
+    bag[key] = {};
+  }
+
+  if(!bag[key][id]) {
+    bag[key][id] = denormalize(entities[key][id], entities, schema, bag);
+  }
+
+  return bag[key][id];
 }
 
-function denormalizeUnion(entity, entities, schema) {
+function denormalizeArray(items, entities, schema, bag) {
+  const itemSchema = schema.getItemSchema();
+  const itemKey = itemSchema.getKey();
+  return items.map(id => getItem(id, itemKey, itemSchema, entities, bag));
+}
+
+function denormalizeUnion(entity, entities, schema, bag) {
   const itemSchema = schema.getItemSchema();
   return denormalize(
     Object.assign({}, entity, { [entity.schema]: entity.id }),
     entities,
-    itemSchema
+    itemSchema,
+    bag
   )[entity.schema];
 }
 
 export function denormalize(entity, entities, entitySchema, bag = {}) {
-  const denormalized = {};
+  const denormalized = merge({}, entity);
   if (entitySchema instanceof UnionSchema) {
-    return denormalizeUnion(entity, entities, entitySchema);
+    return denormalizeUnion(entity, entities, entitySchema, bag);
   }
+
+  if (entitySchema instanceof EntitySchema) {
+    const key = entitySchema.getKey();
+    const id = denormalized[entitySchema.getIdAttribute()];
+    bag[key] = Object.assign(bag[key] || {}, {[id]: denormalized});
+  }
+
   Object.keys(entitySchema)
     .filter(attribute => attribute.substring(0, 1) !== "_")
     .filter(attribute => !!entity[attribute])
@@ -34,15 +51,14 @@ export function denormalize(entity, entities, entitySchema, bag = {}) {
       const itemId = entity[attribute];
 
       if (entitySchema[attribute] instanceof ArraySchema) {
-        denormalized[attribute] = denormalizeArray(itemId, entities, entitySchema[attribute]);
+        denormalized[attribute] = denormalizeArray(itemId, entities, entitySchema[attribute], bag);
       } else if (entitySchema[attribute] instanceof EntitySchema) {
         const itemSchema = entitySchema[attribute];
         const itemKey = itemSchema.getKey();
-        const item = entities[itemKey][itemId];
-        denormalized[attribute] = denormalize(item, entities, itemSchema);
+        denormalized[attribute] = getItem(itemId, itemKey, itemSchema, entities, bag);
       }
 
     });
 
-  return merge({}, entity, denormalized);
+  return denormalized;
 }
