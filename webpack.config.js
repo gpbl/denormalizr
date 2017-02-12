0ce8905355b5a92cef8834540f0bbece21a9e28b
Giampaolo Bellavite <gpbellavite@gmail.com>
2017-02-12 11:26:51 -0300

    Fix build scripts for unpkg

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,18 @@
 /* eslint-env node */
 
+var isProduction = process.env.NODE_ENV === 'production';
+var filename = `denormalizr${isProduction ? '.min' : ''}.js`;
+
 module.exports = {
-  entry: './lib/index.js',
-  devtool: 'source-map',
+  entry: './dist/src/index.js',
   output: {
     path: `${__dirname}/dist`,
-    filename: 'denormalizr.min.js',
+    filename: filename,
     library: 'denormalizr',
     libraryTarget: 'umd',
   },
   externals: {
-    react: {
+    normalizr: {
       root: 'normalizr',
       commonjs2: 'normalizr',
       commonjs: 'normalizr',
